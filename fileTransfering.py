4ec4f3aca4dae267759acd99b03788a846b23802
Mark Arcedas <63905768+MarkusSacred@users.noreply.github.com>
2025-06-18 11:33:53 +0800

    Update fileTransfering.py
    Update file

--- fileTransfering.py
+++ fileTransfering.py
@@ -1,15 +1,19 @@
 import os
 import shutil
 
-source_path_folder = r"C:\Users\215MArcedas\Desktop\fileFolder"
-destination_path_folder = r"C:\Users\215MArcedas\Desktop\destinationFolder"
-file_name = "sample.txt"
+source_path_folder = r"Your originate path folder"
+destination_path_folder = r"Your destination path folder"
+file_name = "Your target file"
 
+#Join of the folder and file path
 full_source_path = os.path.join(source_path_folder, file_name)
 
+#Conditional statement if the folder is not created before taking an action of file transfering
 if not os.path.exists(source_path_folder):
     os.makedirs(source_path_folder)
     print(f"Source folder {source_path_folder} created.")
+    shutil.move(full_source_path, destination_path_folder)
+    print(f"File {file_name} moved to {destination_path_folder}")
 else:
     shutil.move(full_source_path, destination_path_folder)
     print(f"File {file_name} moved to {destination_path_folder}")
